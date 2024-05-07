9fde4bffca9c3ac212106c81f0932a30de9d5bbd
NathaliaVenturini2603 <venturininathalia275@gmail.com>
2024-05-07 11:12:54 -0300

    exercicio_4.py

---
+++ entrada_saida/exercicio_4.py
@@ -0,0 +1,5 @@
+nota1 = int(input("Insira a primeira nota: "))
+nota2 = int(input("Insira a segunda nota: "))
+nota3 = int(input("Insira a terceira nota: "))
+media = (nota1+nota2+nota3) / 2 
+print(f"A media final é: {media}")
