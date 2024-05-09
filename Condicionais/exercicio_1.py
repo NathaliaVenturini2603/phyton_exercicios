9ca6778f32a060d17aab58dde861a7b92b532516
NathaliaVenturini2603 <venturininathalia275@gmail.com>
2024-05-09 16:09:40 -0300

    Atualizaçãoexercicio_1.py

--- Condicionais/exercicio_1.py
+++ Condicionais/exercicio_1.py
@@ -14,7 +14,28 @@ try:
     # Ler o conteudo da resposta 
     data = response.read().decode('utf-8')
 
-    # Teste...
-    print(data)
-except Exception as e: 
-    print(f"Erro: {e}")
+  # Converter JSON em dicionario Python
+  endereco = json.loads(data)
+
+  # Verificar se a consulta foi bem-sucedida 
+if endereco.get('erro'):
+   print("CEP não encontrado")
+  else: 
+   # Armazenar informação em variaveis 
+   logradouro = endereco['logradouro']
+   complemento = endereco['complemento']
+   bairro = endereco['bairro']
+   cidade = endereco['localidade']
+   estado = endereco['uf']
+
+   # Exibir informações do endereço na tela 
+   print(f"logradouro: {logradouro}")
+   print(f"Complemento: {complemento}")
+   print(f"Bairro: {bairro}")
+   print(f"Cidade: {cidade}")
+   print(f"Estado: {estado}")
+
+# Fechar conexão
+ response.close()
+ except Exception as e: 
+   print(f"Erro: {e}")
